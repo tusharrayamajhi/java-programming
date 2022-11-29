ca82e99d16d2eb9d99e2e50e2fea529420f9e828
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2022-11-29 16:45:12 +0545

    update first.java

--- first.java
+++ first.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class first {
-    public static void sum(int a, int b){
+    public static void sum1(int a, int b){
         System.out.println(a + b);
     }
 public static void main(String[] args) {
     Scanner r = new Scanner(System.in);
     int a = r.nextInt();
     int b = r.nextInt();
-    sum(b, a);
+    sum1(b, a);
 }
     
 }
