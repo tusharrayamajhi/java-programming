fa3a84f887714362d6159e94f56b20c4a085d7be
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2022-11-30 10:58:37 +0545

    Update first.java

--- first.java
+++ first.java
@@ -1,20 +1,42 @@
 import java.util.Scanner;
 
+import javax.swing.plaf.metal.MetalBorders.PopupMenuBorder;
+
 public class first {
-    public static void sum1(int marks[]){
+    public static void sum1(int marks[]) {
         int total = 0;
-        for(int i = 0; i <= marks.length - 1; i++){
+        for (int i = 0; i <= marks.length - 1; i++) {
             total = total + marks[i];
         }
         System.out.println(total);
     }
-public static void main(String[] args) {
-    int marks[] = new int[100];
-    marks[0] = 7;
-    marks[1] = 4;
-    marks[2] = 5;
-    sum1(marks);
 
-}
-    
+    public static void LinearSearch(int num[], int number) {
+        for (int i = 0; i <= num.length - 1; i++) {
+            if (num[i] == number) {
+                System.out.println("found at:- " + i);
+            }
+        }
+    }
+
+    public static void Largestamongarray(int num[]) {
+        int inf = Integer.MIN_VALUE;
+        for (int i = 0; i <= num.length - 1; i++) {
+
+            if (num[i] > inf) {
+                inf = num[i];
+            }
+        }
+        System.out.println(inf);
+    }
+
+    public static void main(String[] args) {
+        int marks[] = { 3, 4, 5, 9, 11, 2, 1 };
+        // sum1(marks);
+        // LinearSearch(marks, 4);
+        
+        Largestamongarray(marks);
+
+    }
+
 }
