b418938b0f3971db9b504c503ffdfc8023811fae
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2022-11-30 11:09:38 +0545

    Create uniquenum.java

---
+++ uniquenum.java
@@ -0,0 +1,19 @@
+public class uniquenum {
+    public static boolean num1(int num[]){
+        for(int i = 0; i <= num.length - 1;i++){
+            for(int j = i + 1; j <= num.length - 1; j++){
+                 if(num[i] == num[j]){
+                return true;
+            }
+            }
+           
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int num[] = {1, 2, 3, 4, 9, 8, 5, 6, 7};
+        
+        System.out.println(num1(num));
+
+    }
+}
