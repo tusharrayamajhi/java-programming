c69571f0339fa66020bf0df913d4c73407c74cde
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2022-12-01 10:00:06 +0545

    Update reverseofarray.java

--- reverseofarray.java
+++ reverseofarray.java
@@ -12,7 +12,7 @@ public class reverseofarray {
         
     }
     public static void main(String[] args) {
-        int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int num[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         reverseofarr(num);
         for(int i = 0; i < num.length; i++){
             System.out.println(num[i]);
