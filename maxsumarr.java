88a02d3a7a0f6589328e63e26c1196bc400f44a5
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2022-12-01 10:48:00 +0545

    Create maxsumarr.java

---
+++ maxsumarr.java
@@ -0,0 +1,27 @@
+import java.nio.charset.StandardCharsets;
+
+public class maxsumarr {
+    public static void maxsum(int arr[]){
+        //int max = 0;
+        int sum = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i; j <= arr.length-1; j++){
+                int max = 0;
+                for(int k = i;k <= j; k++){
+                    System.out.print(arr[k]);
+                    max += arr[k];
+                }
+                System.out.println("=" + max);
+                if(sum < max){
+                    sum = max;
+                }
+            }
+        }
+        System.out.println( " max sum =" +  sum);
+    }
+    public static void main(String[] args) {
+         int arr[] = {2, 3, 4, 5, 6, 7, 8, 9};
+         maxsum(arr);
+
+    }
+}
