9c9c2c72e48d285082c4e0ffc802e605d305a7b3
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2022-12-04 11:46:54 +0545

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -1,7 +1,7 @@
 public class bubblesort {
     public static void sort(int arr[]) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
+        for (int i = 0; i <= arr.length; i++) {
+            for (int j = 0; j < arr.length- 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int tem = arr[j];
                     arr[j] = arr[j + 1];
