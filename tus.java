a9d06d807bb946cec0b9e951ad0c548f88c02b23
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2022-12-04 10:16:03 +0545

    Update tus.java

--- tus.java
+++ tus.java
@@ -19,19 +19,20 @@ public class tus {
         
     }
     public static void main(String[] args) {
-        Scanner r = new Scanner(System.in);
-        int[] nums = {7, 5, 3, 4, 7};
-        System.out.print("enter a target=");
-        int target = r.nextInt();
-        int[] num = tar(nums, target);
-        for(int i = 0; i <= num.length; i++){
-            if(num[i] == 0 && num[i + 1] == 0){
-                System.out.println("sorry not found!");
-            }else{
-                System.out.print(num[i] + ",");
+        try (Scanner r = new Scanner(System.in)) {
+            int[] nums = {7, 5, 3, 4, 7};
+            System.out.print("enter a target=");
+            int target = r.nextInt();
+            int[] num = tar(nums, target);
+            for(int i = 0; i <= num.length; i++){
+                if(num[i] == 0 && num[i + 1] == 0){
+                    System.out.println("sorry not found!");
+                }else{
+                    System.out.print(num[i] + ",");
+                }
+                
+               
             }
-            
-           
         }
 
     }
