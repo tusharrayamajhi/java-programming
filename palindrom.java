feb0f4624d3ced1a1979b310610118631511dd5d
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2022-12-06 16:44:44 +0545

    Create palindrom.java

---
+++ palindrom.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public static boolean ispalindrom(String str) {
+        int l = str.length() / 2;
+        for (int i = 0; i <= l; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "noon";
+        System.out.println(ispalindrom(str)); 
+    }
+}
